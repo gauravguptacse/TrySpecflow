8f48caae05480e1ab36ecfa2c02ceaeedea14dbe
Gaurav Gupta <tech_gaurav@hotmail.com>
2014-09-12 13:58:50 +0100

    Added projects

---
+++ OperationOnNumber/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationOnNumber
+{
+    public class Helper
+    {
+        public static int AddNumbers(List<int> numbers)
+        {
+            int sum = 0;
+
+            numbers.ForEach(a => sum += a);
+
+            return sum;
+        }
+    }
+}
---
+++ SpecFlowTestProj/SpecFlowFeatureSteps.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OperationOnNumber;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTestProj
+{
+    [Binding]
+    public class SpecFlowFeatureSteps
+    {
+        private static List<int> numbers = new List<int>();
+        private static int sum;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            numbers.Add(p0);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            sum = Helper.AddNumbers(numbers);
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(120, sum);
+        }
+    }
+}
