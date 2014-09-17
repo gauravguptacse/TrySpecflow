de6d7763ed5d58b5a1fec265e709ef461a5e0286
gauravguptacse <tech_gaurav@hotmail.com>
2014-09-17 12:20:02 +0100

    Update BuildVerifyBatch.bat

--- SpecFlowTestProj/BuildVerifyBatch.bat
+++ SpecFlowTestProj/BuildVerifyBatch.bat
@@ -1,9 +1,9 @@
-"..\packages\NUnit.Runners.2.6.0.12051\tools\nunit-console.exe" /labels /out=SpecFlowResult.txt /xml=SpecFlowResult.xml /framework=net-4.0 bin\Debug\SpecFlowTestProj.dll
+"..\packages\NUnit.Runners.2.6.0.12051\tools\nunit-console.exe" /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.0 bin\Debug\SpecFlowTestProj.dll
  
 "..\packages\SpecFlow.1.9.0\tools\specflow.exe" nunitexecutionreport SpecFlowTestProj.csproj
  
-IF NOT EXIST SpecFlowResult.xml GOTO FAIL
-IF NOT EXIST SpecFlowResult.txt GOTO FAIL
+IF NOT EXIST TestResult.xml GOTO FAIL
+IF NOT EXIST TestResult.html GOTO FAIL
 EXIT
  
 :FAIL
