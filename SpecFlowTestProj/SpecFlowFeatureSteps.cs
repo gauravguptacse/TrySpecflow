0e4dce2b7e9ca64617abce3a794be01106545030
Gaurav Gupta <tech_gaurav@hotmail.com>
2014-09-17 10:27:29 +0100

    Removed error

--- SpecFlowTestProj/SpecFlowFeatureSteps.cs
+++ SpecFlowTestProj/SpecFlowFeatureSteps.cs
@@ -28,7 +28,7 @@ namespace SpecFlowTestProj
         [Then(@"the result should be (.*) on the screen")]
         public void ThenTheResultShouldBeOnTheScreen(int p0)
         {
-            Asser.AreEqual(130, sum);
+            Assert.AreEqual(130, sum);
         }
     }
 }
