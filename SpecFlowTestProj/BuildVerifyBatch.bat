b26d0e43129554945663504c931033ebed8b8ef7
Gaurav Gupta <tech_gaurav@hotmail.com>
2014-09-18 10:04:47 +0100

    Updated batch file to export test result.

--- SpecFlowTestProj/BuildVerifyBatch.bat
+++ SpecFlowTestProj/BuildVerifyBatch.bat
@@ -1,6 +1,6 @@
 "..\packages\NUnit.Runners.2.6.0.12051\tools\nunit-console.exe" /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.0 bin\Debug\SpecFlowTestProj.dll
  
-"..\packages\SpecFlow.1.9.0\tools\specflow.exe" nunitexecutionreport SpecFlowTestProj.csproj
+"..\packages\SpecFlow.1.9.0\tools\specflow.exe" nunitexecutionreport SpecFlowTestProj.csproj /out:SpecflowTestResult.html
  
 IF NOT EXIST TestResult.xml GOTO FAIL
 IF NOT EXIST TestResult.html GOTO FAIL
