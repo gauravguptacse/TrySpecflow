8bfba9f355cef959e567391f2c0325e68e476d32
Gaurav Gupta <tech_gaurav@hotmail.com>
2014-09-19 08:12:09 +0100

    Added and error in build

--- Common/Helper.cs
+++ Common/Helper.cs
@@ -19,7 +19,7 @@ namespace Common.Helper
 
         public static int MultiplyNumbers(List<int> numbers)
         {
-            int product = 1;
+            in product = 1;
 
             numbers.ForEach(a => product *= a);
 
