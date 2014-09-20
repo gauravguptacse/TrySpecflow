422334250493b5346936ed89f0082098f712fe72
gauravguptacse <tech_gaurav@hotmail.com>
2014-09-20 19:30:22 +0530

    Update BuildVerifyBatch.bat

--- SpecFlowTestProj/BuildVerifyBatch.bat
+++ SpecFlowTestProj/BuildVerifyBatch.bat
@@ -3,7 +3,7 @@
 "..\packages\SpecFlow.1.9.0\tools\specflow.exe" nunitexecutionreport SpecFlowTestProj.csproj /out:SpecflowTestResult.html
  
 IF NOT EXIST TestResult.xml GOTO FAIL
-IF NOT EXIST TestResult.html GOTO FAIL
+IF NOT EXIST SpecflowTestResult.html GOTO FAIL
 EXIT
  
 :FAIL
